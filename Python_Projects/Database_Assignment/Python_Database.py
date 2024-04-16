a2c824cf4ccf18f134d207b8038395a98ff86a63
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-04-16 09:06:41 -0500

    Update Python_Database.py
    Made corrections for re submission

--- Python_Projects/Database_Assignment/Python_Database.py
+++ Python_Projects/Database_Assignment/Python_Database.py
@@ -4,37 +4,21 @@ conn = sqlite3.connect('python.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS tbl_persons( \
-        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-        col_fname TEXT, \
-        col_lname TEXT, \
-        col_email TEXT \
-        )")
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_files(ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+                file_name TEXT)")
     conn.commit()
-conn.close()
-
 
 conn = sqlite3.connect('python.db')
 
-with conn:
-    cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_persons(col_fname, col_lname, col_email) VALUES (?,?,?)", \
-                ('Billy', 'Bob', 'BillyB@gmail.com'))
-    cur.execute("INSERT INTO tbl_persons(col_fname, col_lname, col_email) VALUES (?,?,?)", \
-                ('May', 'Sally', 'MaySal@gmail.com'))
-    cur.execute("INSERT INTO tbl_persons(col_fname, col_lname, col_email) VALUES (?,?,?)", \
-                ('Lebron', 'James', 'LBJ@gmail.com'))
-    conn.commit()
+fileList = ('information.docx', 'Hello.txt','myImage.png', \
+            'myMovie.mpg', 'World.txt', 'data.pdf','myPhoto.jpg')
+
+for x in fileList:
+    if x.endswith('txt'):
+        with conn:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO tbl_files (file_name) VALUES (?)", (x,))
+            print(x)
+
 conn.close()
 
-
-conn = sqlite3.connect('python.db')
-
-with conn:
-    cur = conn.cursor()
-    cur.execute("SELECT col_fname,col_lname,col_email FROM tbl_persons WHERE col_fname = 'May'")
-    varPerson = cur.fetchall()
-    for item in varPerson:
-        msg = "First Name: {}\nLast Name: {}\nEmail: {}".format(item[0],item[1],item[2])
-    print(msg)
-conn.close()
