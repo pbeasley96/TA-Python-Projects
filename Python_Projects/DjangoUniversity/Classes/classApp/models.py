c9c2607dc4a3d3e3b11dd622a6ecce0f6753772b
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-05-13 17:22:47 -0500

    DjangoUniversity_Project
    Created and set up the basics for the DjangoUniversity project.

---
+++ Python_Projects/DjangoUniversity/Classes/classApp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class UniversityClasses(models.Model):
+    title = models.CharField(max_length=60, default="", blank=True, null=False)
+    course_number = models.IntegerField(default="", blank=True, null=False)
+    instructor_name = models.CharField(max_length=60, default="", blank=True, null=False)
+    duration = models.FloatField(null=True, blank=True, default=None)
+
+    object = models.Manager()
+
+    def __str__(self):
+        display_course = '{0.title}: {0.instructor_name}'
+        return display_course.format(self)
+
+    class Meta:
+        verbose_name_plural = "University Classes"
